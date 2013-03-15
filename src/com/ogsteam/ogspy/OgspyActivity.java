16c8befad1cacac536abf1b09900c9f221d2cde3
Jean-Philippe <jedi-night@users.noreply.github.com>
2013-03-15 17:12:25 +0100

    Page des r?glages

--- src/com/ogsteam/ogspy/OgspyActivity.java
+++ src/com/ogsteam/ogspy/OgspyActivity.java
@@ -1,12 +1,14 @@
-package com.example.myfirstapp;
+package com.ogsteam.ogspy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
-
+public class OgspyActivity extends Activity {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,14 +27,23 @@ public class MainActivity extends Activity {
 	    // Handle item selection
 	    switch (item.getItemId()) {
 	        case R.id.settings:
-	            settings();
+	            showSettings();
 	            return true;
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
 	
-	public void settings() {
+	/** Display the settings windows */
+	public void showSettings() {
 		setContentView(R.layout.settings);
+	}
+	
+	/** Called when the user clicks the Save button in Settings*/
+	public void saveSettings(View view) {
+		String userOgspy = ((EditText) findViewById(R.id.ogspy_user)).getText().toString();
+		String password = ((EditText) findViewById(R.id.ogspy_password)).getText().toString();
+		String serverUrl = ((EditText) findViewById(R.id.ogspy_server_url)).getText().toString();
+		
 	}
 }
