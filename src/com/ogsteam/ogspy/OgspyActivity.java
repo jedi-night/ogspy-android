ba9e6d47d77ebadb5c5f4531453bc333fafe4e44
Jean-Philippe <jedi-night@users.noreply.github.com>
2013-03-20 17:42:41 +0100

    Ajout du t?l?chargement des donn?es d'OGSPY

--- src/com/ogsteam/ogspy/OgspyActivity.java
+++ src/com/ogsteam/ogspy/OgspyActivity.java
@@ -1,7 +1,17 @@
 package com.ogsteam.ogspy;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,10 +20,15 @@ import android.widget.Toast;
 
 import com.ogsteam.ogspy.data.DatabaseAccountHandler;
 import com.ogsteam.ogspy.data.models.Account;
+import com.ogsteam.ogspy.utils.Constants;
+import com.ogsteam.ogspy.utils.StringUtils;
 
 public class OgspyActivity extends Activity {
-	private DatabaseAccountHandler handler;
+	private static final String DEBUG_TAG = "OgspyActivity";
 	
+	// Variables
+	private DatabaseAccountHandler handler;
+	protected static String dataFromAsyncTask; 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -112,5 +127,79 @@ public class OgspyActivity extends Activity {
 			Toast.makeText(this, sb.toString(), Toast.LENGTH_SHORT).show();
 		}
 		return status;
+	}
+	
+	// Given a URL, establishes an HttpUrlConnection and retrieves
+	// the web page content as a InputStream, which it returns as
+	// a string.
+	private String downloadUrl(String myurl) throws IOException {
+	    InputStream is = null;
+	    // Only display the first 500 characters of the retrieved
+	    // web page content.
+	    int len = 500;
+	        
+	    try {
+	        URL url = new URL(myurl);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setReadTimeout(10000 /* milliseconds */);
+	        conn.setConnectTimeout(15000 /* milliseconds */);
+	        conn.setRequestMethod("GET");
+	        conn.setDoInput(true);
+	        // Starts the query
+	        conn.connect();
+	        int response = conn.getResponseCode();
+	        Log.d(DEBUG_TAG, "The response is: " + response);
+	        is = conn.getInputStream();
+
+	        // Convert the InputStream into a string
+	        String contentAsString = readIt(is, len);
+	        return contentAsString;
+	    // Makes sure that the InputStream is closed after the app is
+	    // finished using it.
+	    } finally {
+	        if (is != null) {
+	            is.close();
+	        } 
+	    }
+	}
+	
+	// Reads an InputStream and converts it to a String.
+	public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+	    Reader reader = null;
+	    reader = new InputStreamReader(stream, "UTF-8");        
+	    char[] buffer = new char[len];
+	    reader.read(buffer);
+	    return new String(buffer);
+	}
+	
+	private class MyAsyncTask extends AsyncTask<String, Integer, String> {
+	    private Activity activity;
+
+	    public MyAsyncTask(Activity activity) {
+	        this.activity = activity;
+	    }
+
+	    @Override
+		protected String doInBackground(String... params) {
+			try {
+				if(!handler.getAllAccounts().isEmpty()){
+					Account account = handler.getAccountById(0);
+					String url = StringUtils.formatPattern(Constants.URL_GET_OGSPY_INFORMATION, account.getUsername(), account.getPassword());
+					dataFromAsyncTask = downloadUrl(url);
+				}
+			} catch (Exception e) {
+				Log.e(DEBUG_TAG, "Prioblème lors du telechargement");
+			}
+			return null;
+		}
+
+	     protected void onProgressUpdate(Integer... progress) {
+	         //setProgressPercent(progress[0]);
+	     }
+
+		protected void onPostExecute(String result) {
+	    	((EditText) findViewById(R.id.response_ogspy)).setText(dataFromAsyncTask);    	
+		}
+
 	}
 }
---
+++ src/com/ogsteam/ogspy/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.ogsteam.ogspy.utils;
+
+public interface Constants {
+	public static final String URL_GET_OGSPY_INFORMATION = "mod/xtense/xtense.php?toolbar_version=2.5.2&toolbar_type=FF&mod_min_version=2.4.1&user={0}&password={1}";
+}
