e0015fa6d4ed5e5f3c54963a67eff15eac440672
Jean-Philippe <jedi-night@users.noreply.github.com>
2013-07-05 16:25:29 +0200

    V 1.2.0

---
+++ src/com/ogsteam/ogspy/preferences/Preferences.java
@@ -0,0 +1,61 @@
+package com.ogsteam.ogspy.preferences;
+
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.ogsteam.ogspy.OgspyActivity;
+import com.ogsteam.ogspy.R;
+import com.ogsteam.ogspy.data.models.Prefs;
+import com.ogsteam.ogspy.fragments.tabs.PrefsFragment;
+import com.ogsteam.ogspy.utils.OgspyUtils;
+
+public class Preferences {
+
+	/** Display the account windows */
+	public static void showPrefs(OgspyActivity activity, PrefsFragment fragment) {
+		//activity.setContentView(R.layout.prefs);
+		Spinner spinner = fragment.getTimerHostiles();
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(activity, R.array.prefs_timer_hostiles, android.R.layout.simple_spinner_item);
+		
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		if(spinner != null && adapter != null) {
+            spinner.setAdapter(adapter);
+        }
+		
+		if(!activity.getHandlerPrefs().getAllPrefs().isEmpty()){
+			Prefs prefs = activity.getHandlerPrefs().getPrefsById(0);
+			if(prefs != null && prefs.getRefreshHostiles() > 0){
+                int value = prefs.getRefreshHostiles();
+                if(spinner != null) {
+                    spinner.setSelection(value);
+                }
+			}
+		}
+	}
+	
+	/** Called when the user clicks the Save button in account*/
+	public static void savePrefs(OgspyActivity activity) {
+		int selectedId = PrefsFragment.getTimerHostiles().getSelectedItemPosition();
+
+		if(activity.getHandlerPrefs().getPrefsCount() > 0){
+			activity.getHandlerPrefs().deleteAllPrefs();
+		}
+		if(activity.getHandlerPrefs().addPrefs(new Prefs(0, selectedId)) != -1){
+			Toast.makeText(activity, activity.getString(R.string.save_prefs_ok), Toast.LENGTH_LONG).show();
+			// Affectation du timer
+			OgspyActivity.setTimer(OgspyUtils.getTimerHostiles(activity, activity.getHandlerPrefs()));
+			// Changement du check hostiles suivant le timer
+			if(activity.autoUpdateHostiles != null){
+				activity.autoUpdateHostiles.cancel();
+				activity.autoUpdateHostiles.purge();
+				activity.setAutomaticCheckHostiles();
+			}
+		} else {
+			Toast.makeText(activity, activity.getString(R.string.save_prefs_ko), Toast.LENGTH_LONG).show();
+		}
+	}
+}
